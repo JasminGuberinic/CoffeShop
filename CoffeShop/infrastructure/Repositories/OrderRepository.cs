c678a7be5af800648358f9f01ee2ab0f17a0598c
JasminGuberinic <jasmin.guberinic@gmail.com>
2020-05-09 17:49:27 +0200

    Infrastructure

---
+++ CoffeShop/infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,37 @@
+﻿using Domain;
+using Framework;
+using System;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace infrastructure
+{
+    public class OrderRepository
+    {
+        EventStore eventStore = new EventStore();
+        public Order LoadAsync(string aggregateID)
+        {
+            var commitedevents = eventStore.LoadEvents(aggregateID);
+            var domainevents = commitedevents.Select(Transform);
+            return new Order(domainevents);
+        }
+
+        public async Task SaveAsync(Order order)
+        {
+            await SaveAggregateAsync(order.GetType().Name, order.Id.Value.ToString(), order.Version, order.GetChanges());
+        }
+
+        private async Task SaveAggregateAsync(string aggregateName, string aggregateID, int version, IEnumerable<DomainEvent> domainEvents)
+        {
+            await eventStore.SaveEvents(domainEvents, version, aggregateID, aggregateName);
+        }
+
+        private DomainEvent Transform(CommitedEvent commitedEvent)
+        {
+            var e = JsonConvert.DeserializeObject<DomainEvent>(commitedEvent.MetaData);
+            return e;
+        }
+    }
+}
